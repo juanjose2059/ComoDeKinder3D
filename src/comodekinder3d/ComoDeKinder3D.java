6008fe75fe3bc979c4b7a4d35976f56d135312b6
Juan José Cruz Cornejo <juan.cruz.sc@itszapopan.edu.mx>
2018-10-02 18:10:03 -0500

    Segunda parte

--- src/comodekinder3d/ComoDeKinder3D.java
+++ src/comodekinder3d/ComoDeKinder3D.java
@@ -17,6 +17,7 @@ public class ComoDeKinder3D {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        System.out.println("Nuevo");
     }
     
 }

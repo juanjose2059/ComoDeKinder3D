80aac59bdb108da5d0ed6d87c8b6c6b3945984bf
Juan José Cruz Cornejo <juan.cruz.sc@itszapopan.edu.mx>
2018-10-08 17:42:21 -0500

    try catch

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -4,17 +4,25 @@
  * and open the template in the editor.
  */
 package renderEngine;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 /**
  *
  * @author JUAN
  */
 public class DisplayManager {
-    public static void createDisplay(){
-        private static final int WIDTH =1280;
-        private static final int HEIGHT = 720;
+    
+        private static final int WIDTH = 1024;
+        private static final int HEIGHT = 768;
         
-        Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
+    public static void createDisplay(){
+        try{
+            Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));            
+        }catch (LWJGLException e) {
+            e.printStackTrace();
+        }        
     }
     public static void updateDisplay(){}
     public static void closeDisplay(){}
